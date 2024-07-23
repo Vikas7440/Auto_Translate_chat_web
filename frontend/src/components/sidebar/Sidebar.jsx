fb7c548500530186921bddad18dbc8cb2cdf8b28
Vikas Mandal <vikasmandal8055@gmail.com>
2024-07-23 23:15:13 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,35 +1,19 @@
+import React from 'react';
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 import Side from "./Side";
+
 const Sidebar = () => {
 	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'
-		style={{ width: "100%" }}
-		>
+		<div className='border-r border-slate-500 p-4 flex flex-col w-full md:w-1/4'>
 			<Side />
 			<SearchInput />
-			<div className='divider px-3'></div>
+			<div className='divider px-3 my-4'></div>
 			<Conversations />
 			<LogoutButton />
 		</div>
 	);
 };
+
 export default Sidebar;
-
-// STARTER CODE FOR THIS FILE
-// import Conversations from "./Conversations";
-// import LogoutButton from "./LogoutButton";
-// import SearchInput from "./SearchInput";
-
-// const Sidebar = () => {
-// 	return (
-// 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-// 			<SearchInput />
-// 			<div className='divider px-3'></div>
-// 			<Conversations />
-// 			<LogoutButton />
-// 		</div>
-// 	);
-// };
-// export default Sidebar;
